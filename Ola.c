356f30b94c81f1306e4bb6df8ce926608e13e1d2
Rafael Corrêa Zart <rafaelcorreazart@gmail.com>
2022-05-10 18:12:35 -0300

    Solicita novo valor

--- Ola.c
+++ Ola.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    int x;
+    int x, x2;
     
     printf("Olar mundo\n");
     printf("Digite um numero: ");
     scanf("%d", &x);
+    printf("Digite um numero positivo: ");
+    scanf("%d", &x2);
 }
