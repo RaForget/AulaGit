d87d5eece7e2830b12404d44e4fbeb09ac0823c9
Rafael Corrêa Zart <rafaelcorreazart@gmail.com>
2022-05-10 18:02:55 -0300

    Nova funcionalidade

--- Ola.c
+++ Ola.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    int x;
+    int x, x1;
     
     printf("Olar mundo\n");
     printf("Digite um numero: ");
     scanf("%d", &x);
+    printf("Digite um numero negativo: ");
+    scanf("%d", &x1);
 }
